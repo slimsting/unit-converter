6cd98188581e8a7dbfc133cac2fe219cff4913ae
slimsting <newbon.gadson@gmail.com>
2023-05-28 02:02:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,61 +3,65 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
-const meterFeet = 3.281
-const literGallon = 0.264
-const kilogramPound = 2.204
-const button = document.getElementById('btn-el')
+//convertion ratios
+const meterFeet = 3.281;
+const literGallon = 0.264;
+const kilogramPound = 2.204;
 
-button.addEventListener("click", function(){
-    
-    const inputEl = document.getElementById("entry").value 
-    const lengthEl = document.getElementById("length")  
-    const volumeEl = document.getElementById("volume") 
-    const massEl = document.getElementById("mass")  
-    const message = document.getElementById("message")
-    
-    message.textContent = ""
-    
-    if(inputEl){
-        if(isNaN(inputEl)){
-            message.textContent = "please enter a number!"
-        }
-        else{
-            lengthEl.innerHTML = `${inputEl} meters = ${metersToFeet(inputEl)} feet | 
-                        ${inputEl} feet = ${feetToMeters(inputEl)} meters`
-            volumeEl.innerHTML = `${inputEl} liters = ${litersToGallons(inputEl)} gallons | 
-                            ${inputEl} gallons = ${gallonsToLiters(inputEl)} liters`
-            massEl.innerHTML = `${inputEl} kilos = ${kilogramsToPounds(inputEl)} pounds | 
-                            ${inputEl} pounds = ${poundsToKilograms(inputEl)} kilos`
-        }
-        
+const buttonEl = document.getElementById("btn-el");
+const inputEl = document.getElementById("entry");
+const lengthEl = document.getElementById("length");
+const volumeEl = document.getElementById("volume");
+const massEl = document.getElementById("mass");
+const messageEl = document.getElementById("message");
+
+buttonEl.addEventListener("click", function () {
+  const input = inputEl.value;
+  messageEl.textContent = "";
+
+  if (input) {
+    if (
+      isNaN(input)
+    ) {
+      messageEl.textContent = "please enter a number!";
     }
-    else{
-        message.textContent = "please enter a value!"
+    // prettier-ignore
+    else {
+      lengthEl.innerHTML = `${input} meters = ${metersToFeet(
+        input
+      )} feet | ${input} feet = ${feetToMeters(input)} meters`;
+      volumeEl.innerHTML = `${input} liters = ${litersToGallons(
+        input
+      )} gallons | ${input} gallons = ${gallonsToLiters(input)} liters`;
+      massEl.innerHTML = `${input} kilos = ${kilogramsToPounds(
+        input
+      )} pounds | ${input} pounds = ${poundsToKilograms(input)} kilos`;
     }
-    
-})
+  } else {
+    messageEl.textContent = "please enter a value!";
+  }
+});
 
-function metersToFeet(num){
-    return (num * meterFeet).toFixed(3)
+function metersToFeet(num) {
+  return (num * meterFeet).toFixed(3);
 }
 
-function feetToMeters(num){
-    return (num / meterFeet).toFixed(3)
+function feetToMeters(num) {
+  return (num / meterFeet).toFixed(3);
 }
 
-function litersToGallons(num){
-    return (num * literGallon).toFixed(3)
+function litersToGallons(num) {
+  return (num * literGallon).toFixed(3);
 }
 
-function gallonsToLiters(num){
-    return (num / literGallon).toFixed(3)
+function gallonsToLiters(num) {
+  return (num / literGallon).toFixed(3);
 }
 
-function kilogramsToPounds(num){
-    return (num * kilogramPound).toFixed(3) 
+function kilogramsToPounds(num) {
+  return (num * kilogramPound).toFixed(3);
 }
 
-function poundsToKilograms (num){
-    return (num / kilogramPound).toFixed(3)
+function poundsToKilograms(num) {
+  return (num / kilogramPound).toFixed(3);
 }
